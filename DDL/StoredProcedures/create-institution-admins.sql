d676b4e8b5256741e38fc2951d26fe148a81da52
Andile Mkhize <Andile.Mkhize@bbd.co.za>
2024-02-08 17:19:35 +0200

    created admin & institution procedures

---
+++ DDL/StoredProcedures/create-institution-admins.sql
@@ -0,0 +1,19 @@
+USE busaryDB
+GO
+
+CREATE PROCEDURE CreateInstitutionAdminsTable
+AS
+BEGIN
+    CREATE TABLE InstitutionAdmins (
+       InstitutionAdminID INT IDENTITY(1,1) NOT NULL,
+       InstitutionID INT NOT NULL,	
+       UserID INT NOT NULL,
+       DepartmentName VARCHAR(120) NOT NULL,
+       Posittion VARCHAR(100) NOT NULL,
+       Email VARCHAR(120) NOT NULL, 
+       InstitutionType VARCHAR(100) NOT NULL,
+       CONSTRAINT PK_InstitutionAdminID PRIMARY KEY (InstitutionAdminID),
+       CONSTRAINT FK_InstitionAdmins_Institutions FOREIGN KEY (InstitutionID) REFERENCES Institutions(InstitutionID),
+       CONSTRAINT FK_InstitionAdmins_Users FOREIGN KEY (UserID) REFERENCES Users(UserID)
+    );
+END;
---
+++ DDL/StoredProcedures/create-institution-table.sql
@@ -0,0 +1,16 @@
+USE busaryDB
+GO
+
+CREATE PROCEDURE CreateInstitutionTable
+AS
+BEGIN
+    CREATE TABLE Institution (
+       InstitutionID INT IDENTITY(1,1) NOT NULL,
+       InstitutionName	VARCHAR(120) NOT NULL,	
+       Email VARCHAR(120) NOT NULL, 
+       InstitutionType VARCHAR(100) NOT NULL,
+       CONSTRAINT PK_Institution PRIMARY KEY (InstitutionID)
+    );
+END;
+
+EXEC dbo.CreateInstitutionTable
