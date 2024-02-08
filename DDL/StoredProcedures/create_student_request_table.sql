c51af899f3da08e9d1d6f3873ae28c49123994fc
mraees93 <raeesdolley76@gmail.com>
2024-02-08 19:20:16 +0200

    Completed CreateStudentRequestTable procedure

--- DDL/StoredProcedures/create_student_request_table.sql
+++ DDL/StoredProcedures/create_student_request_table.sql
@@ -2,8 +2,15 @@ CREATE PROCEDURE CreateStudentRequestTable
 AS
 CREATE TABLE StudentRequests(
     RequestID INT PRIMARY KEY,
-    StudentID INT FOREIGN KEY REFERENCES,
+    StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
     Motivation VARCHAR(MAX),
     Amount MONEY,
-    RequestStatus VARCHAR(20)
+    RequestStatus VARCHAR(20),
+    Approved BOOLEAN,
+    DateOfRequestSent DATE,
+    DateOfStatusUpdate DATE,
+    InstitutionAdminsID INT FOREIGN KEY REFERENCES InstitutionAdmins(InstitutionAdminsID),
+    InstitutionID INT FOREIGN KEY REFERENCES InstitutionAdmins(InstitutionID),
+    BBDAdminID INT FOREIGN KEY REFERENCES BBDAdmins(BBDAdminID)
 )
+GO
