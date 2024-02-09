fbb0b51a1ea47a4e5f0a914f9f3b2dc718212abf
mraees93 <raeesdolley76@gmail.com>
2024-02-09 09:31:41 +0200

    updated email column in users table
    because max allowed chars for email is 254

--- DDL/StoredProcedures/create_users_table.sql
+++ DDL/StoredProcedures/create_users_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE Users(
     LastName VARCHAR(MAX), 
     Username VARCHAR(50), 
     Password CHAR(64), 
-    Email VARCHAR(50), 
+    Email VARCHAR(254), 
     Address VARCHAR(100), 
     RoleID INT FOREIGN KEY REFERENCES Roles(RoleID)
     )
