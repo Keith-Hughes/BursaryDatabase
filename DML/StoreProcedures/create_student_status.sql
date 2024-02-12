4e8086d2add696a55e5a570e4a2d461332533596
mraees93 <raeesdolley76@gmail.com>
2024-02-12 08:43:32 +0200

    removed redundant files

--- DML/StoreProcedures/create_student_status.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE CreateStudentStatus @InstitutionID INT
-AS
-
-SELECT StatusID, StatusName, Amount, FirstName, LastName, InstitutionID
-FROM Statuses, StudentRequests, Students, Institutions
-WHERE InstitutionID = @InstitutionID
-
-GO
